96d270e2ca23c44e905a4b97b3ac9171ef80654f
S M Ali Zaidi <67506359+Zaidi-Ali@users.noreply.github.com>
2020-07-21 12:02:11 +0530

    Add files via upload

---
+++ Day#1 assignment.py
@@ -0,0 +1,25 @@
+Q 1. What is python? what are the ways to install python on your PC?
+
+Answer:-Python is an interpreted, high-level, General-purpose programming language. It is created by "Guido van Rossum" and was first released in 1991." It is a popular general-purpose programming language and can be used for a wide variety of applications. Comparing to any other programming language, Python provides lots of inbuilt libraries which helps the developer to develop an application in an easy way. It became popular in 2006.
+
+•Ways to download Python:
+1. Go to the following link: Anaconda.com/downloads
+2. Select Windows where the three operating systems are listed.
+3. Download the most recent Python 3 release
+
+Installing
+1. Open and run the installer.
+2. Finally, python will be installed.
+3. Click Next to confirm the installation.
+4. Agree to the license.
+5. Install
+
+
+Q 2. What are the code environment and its types?
+Answer: -A code environment is a tool that used to write and edit code. It usually provides features such as built automation, code learning, testing a and debugging.
+
+Types:-There are 3 types of environments which are used mostly:
+
+> Text Editors (sublime text editor)
+> Full IDEs (spyder, pycharm)
+> Notebook Environment (Jupyter notebook)
